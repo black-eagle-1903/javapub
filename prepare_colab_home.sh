b6648d517630b0cc69b2bfc8539eb504df393b5d
Black Eagle Since 1903 <49439840+black-eagle-1903@users.noreply.github.com>
2022-02-11 22:32:04 +0300

    Update prepare_colab_home.sh

--- prepare_colab_home.sh
+++ prepare_colab_home.sh
@@ -10,16 +10,16 @@ rm jdownloader.7z.001 jdownloader.7z.002
 # Oracle JDK
 wget "https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz"
 tar -xvf jdk-17_linux-x64_bin.tar.gz
-rm jdk-17_linux-x64_bin.tar.gz
 
 # Double Commander configuration
 curl -OL "https://github.com/black-eagle-1903/javapub/raw/main/doublecmd_config.7z"
 7z x doublecmd_config.7z -y
+rm doublecmd_config.7z
 
 # Create jdownloader.sh and give execute permision
 printf "cd jdownloader\n"$HOME/$(tar -tf jdk-17_linux-x64_bin.tar.gz|cut -d "/" -f1|sort|uniq)"/bin/java -jar JDownloader.jar">jdownloader.sh
 chmod u+x jdownloader.sh
-rm doublecmd_config.7z
+rm jdk-17_linux-x64_bin.tar.gz
 
 # Create sleep_loop.sh
 echo "for i in {1..14400};do echo -e \".\c\"; sleep 3; done">sleep_loop.sh
