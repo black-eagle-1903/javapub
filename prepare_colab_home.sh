f6662b0c0c1aa6f68d6ca63946940c302f26dff3
Black Eagle Since 1903 <49439840+black-eagle-1903@users.noreply.github.com>
2022-02-11 22:26:03 +0300

    Update prepare_colab_home.sh

--- prepare_colab_home.sh
+++ prepare_colab_home.sh
@@ -15,11 +15,11 @@ rm jdk-17_linux-x64_bin.tar.gz
 # Double Commander configuration
 curl -OL "https://github.com/black-eagle-1903/javapub/raw/main/doublecmd_config.7z"
 7z x doublecmd_config.7z
-rm doublecmd_config.7z
 
 # Create jdownloader.sh and give execute permision
 printf "cd jdownloader\n"$HOME/$(tar -tf jdk-17_linux-x64_bin.tar.gz|cut -d "/" -f1|sort|uniq)"/bin/java -jar JDownloader.jar">jdownloader.sh
 chmod u+x jdownloader.sh
+rm doublecmd_config.7z
 
 # Create sleep_loop.sh
 echo "for i in {1..14400};do echo -e \".\c\"; sleep 3; done">sleep_loop.sh
