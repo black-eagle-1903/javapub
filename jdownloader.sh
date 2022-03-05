0b687826e5b0e0fc3bad80a881037e028c7e4d95
Black Eagle Since 1903 <49439840+black-eagle-1903@users.noreply.github.com>
2022-03-05 22:53:38 +0300

    Delete jdownloader.sh

--- jdownloader.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Bash script for launching JDownloader
-
-cd jdownloader
-# tar -tf jdk-17_linux-x64_bin.tar.gz|cut -d "/" -f1|sort|uniq
-# This command obtains the extracted JDK path dynamically
-"$HOME/$(tar -tf jdk-17_linux-x64_bin.tar.gz|cut -d "/" -f1|sort|uniq)"/bin/java -jar JDownloader.jar
