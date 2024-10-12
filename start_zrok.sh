d057d8c73a4b1c6c7dcaea4deca2c82cdfd46830
Black Eagle Since 1903 <49439840+black-eagle-1903@users.noreply.github.com>
2024-10-12 17:01:11 +0300

    Update start_zrok.sh
    Remove 1st argument support and zrok environment enabling

--- start_zrok.sh
+++ start_zrok.sh
@@ -5,13 +5,13 @@ zrok_package_URL='https://github.com/openziti/zrok/releases/download/'$latest_ve
 zrok_secret_token=$1
 
 # 1st argument is zrok secret token
-if [ -z "${zrok_secret_token}" ]; then
-	echo "Error: zrok secret token is not supplied!"
-	exit 1
-fi
+#if [ -z "${zrok_secret_token}" ]; then
+#	echo "Error: zrok secret token is not supplied!"
+#	exit 1
+#fi
 
 curl -OL "${zrok_package_URL}"
 tar -xvf ./zrok*linux*.tar.gz zrok
 rm zrok*linux*.tar.gz
-./zrok enable ${zrok_secret_token}
+#./zrok enable ${zrok_secret_token}
 ./zrok share private --backend-mode tcpTunnel localhost:5901
