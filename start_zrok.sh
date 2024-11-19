2acc6c2683d757e6c5d441a852b69bc114ab6d56
Black Eagle Since 1903 <49439840+black-eagle-1903@users.noreply.github.com>
2024-11-19 21:06:01 +0300

    Update start_zrok.sh

--- start_zrok.sh
+++ start_zrok.sh
@@ -4,14 +4,14 @@ latest_version=$(basename $(curl -sw "%{redirect_url}" "${zrok_latest_release_UR
 zrok_package_URL='https://github.com/openziti/zrok/releases/download/'$latest_version'/zrok_'${latest_version:1}'_linux_amd64.tar.gz'
 zrok_secret_token=$1
 
-# 1st argument is zrok secret token
-#if [ -z "${zrok_secret_token}" ]; then
-#	echo "Error: zrok secret token is not supplied!"
-#	exit 1
-#fi
+1st argument is zrok secret token
+if [ -z "${zrok_secret_token}" ]; then
+	echo "Error: zrok secret token is not supplied!"
+	exit 1
+fi
 
 curl -OL "${zrok_package_URL}"
 tar -xvf ./zrok*linux*.tar.gz zrok
 rm zrok*linux*.tar.gz
-#./zrok enable ${zrok_secret_token}
+./zrok enable ${zrok_secret_token}
 ./zrok share private --backend-mode tcpTunnel localhost:5901
